70a8767886fe81ce105a920bdb67275a0ed6d6c6
Hugo Cicarelli <hcicarelli@gmail.com>
2024-07-23 19:19:54 -0300

    Explict animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -3,7 +3,7 @@ import 'package:meals_app/data/dummy.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen(
       {super.key, required this.availableMeals, required this.favoriteMeals});
 
@@ -11,23 +11,70 @@ class CategoriesScreen extends StatelessWidget {
   final List<Meal> availableMeals;
 
   @override
+  State<CategoriesScreen> createState() {
+    // TODO: implement createState
+    return _CategoriesScreenState();
+  }
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(16),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    var favoriteMeals = widget.favoriteMeals;
+    var availableMeals = widget.availableMeals;
+
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              favoriteMeals: favoriteMeals,
+              availableMeals: availableMeals,
+            )
+        ],
       ),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            favoriteMeals: favoriteMeals,
-            availableMeals: availableMeals,
-          )
-      ],
+      builder: (ctx, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+              parent: _animationController, curve: Curves.easeInOut),
+        ),
+        child: child,
+      ),
     );
   }
 }
